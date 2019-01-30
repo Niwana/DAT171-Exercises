3f748d5d57eb4c48f8b3037f96bbe6ef0a56d1d2
Niwana <martin.vanky@gmail.com>
2019-01-30 21:15:52 +0100

    Fixed the output from read_coordinate_file
    Changed from a list of strings to a list of floats

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,14 +1,29 @@
-#import time
+import numpy as np
+import time as t
+
 
 
 #start=time.time()
+
+# Read in coordinates from file and convert it into a list
 def read_coordinate_file(filename):
     file1 = open(filename, 'r')
+    npArray = []
     for line in file1:
-        line = line.strip("{}\n ").split(',')
-        print(line)
+        line = line.strip('{\n}')
+        line = line.replace(' ', '')
+        line = line.split(',')
+        #print(line)
+        for item in line:
+            npArray.append(float(item))
+    npArray = np.array(npArray)
+    #np.set_printoptions(threshold=np.nan)      # Prints out all items in a numpy list
+    print(npArray)
+    print(type(npArray))
 
     file1.close()
+
+
 read_coordinate_file('SampleCoordinates.txt')
 
 #end=time.time()
