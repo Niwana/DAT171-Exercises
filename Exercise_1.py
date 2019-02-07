a1b0fd2e037f1f2b2b3d07d535d77f8651ffbd9c
Niwana <martin.vanky@gmail.com>
2019-02-07 12:40:14 +0100

    Add files via upload
    Till och med funktion 7

--- Exercise_1.py
+++ Exercise_1.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection as lc
 import time as t
 from scipy.sparse import csr_matrix as csr
+from scipy.sparse import csgraph
+
 
 R = 1
 start = t.time()
@@ -31,10 +33,16 @@ def read_coordinate_file(filename):
 # print("Tidsåtgång: ", end - start)
 
 
-def plot_points(coords, indices):
+def plot_points(coords, indices, chosenPath):
     line_segments = lc(coords[indices], color='gray', linewidths=0.2)
+
+    segs = coords[chosenPath]
+    #path_segment = lc(segs, color='r', linediwth=1)
+
+    print(segs)
     ax = plt.gca()
     ax.add_collection(line_segments)
+    #ax.add_collection(path_segment)
     plt.scatter(coords[:, 0], coords[:, 1], color='r', s=10)
     plt.show()
 
@@ -42,7 +50,6 @@ def plot_points(coords, indices):
 def construct_graph_connections(coord_list, radius):
     indices = []
     distances = []
-    # distance_fn = dist.pdist(coord_list)
 
     for i, city_i in enumerate(coord_list):
         for j in range(i + 1, len(coord_list)):
@@ -57,10 +64,34 @@ def construct_graph_connections(coord_list, radius):
 
 def construct_graph(indices, costs, N):
     sparseGraph = (csr((costs, (indices[:, 0], indices[:, 1])), shape=(N, N)))
+    #print(sparseGraph)
     return sparseGraph
 
 
-coord_list = read_coordinate_file('HungaryCities.txt')
-indices, costs = construct_graph_connections(coord_list, 0.005)
+def cheapest_path(sparseGraph, start_node, end_node):
+    cost_matrix, predecessor_matrix = csgraph.dijkstra(sparseGraph, directed=False, indices=start_node, return_predecessors=True)
+    return cost_matrix[end_node], predecessor_matrix
+
+
+def compute_path(predecessor_matrix, start_node, end_node):
+    i = end_node
+    path=[i]
+    while i != start_node:
+        path.append(predecessor_matrix[i])
+        i = predecessor_matrix[i]
+    return path[::-1]
+
+coord_list = read_coordinate_file('SampleCoordinates.txt')
+indices, costs = construct_graph_connections(coord_list, 0.08)
 sparseGraph = construct_graph(indices, costs, len(coord_list))
-plot_points(coord_list, indices)
+cost, predecessor_matrix = cheapest_path(sparseGraph, start_node=0, end_node=5)
+chosenPath = compute_path(predecessor_matrix, start_node=0, end_node=5)
+
+#print(chosenPath)
+#print(coord_list)
+#print(coord_list[chosenPath])
+
+plot_points(coord_list, indices, chosenPath)
+#print(csgraph.reconstruct_path(sparseGraph, predecessor_matrix, directed=False))
+help(lc)
+
