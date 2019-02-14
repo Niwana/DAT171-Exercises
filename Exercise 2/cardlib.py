57bcc1dc8ff6477a17e4b496a18ccb07d50dc531
Niwana <martin.vanky@gmail.com>
2019-02-14 12:01:31 +0100

    Add files via upload

--- Exercise 2/cardlib.py
+++ Exercise 2/cardlib.py
@@ -1,4 +1,6 @@
 import enum
+import abc
+import random
 
 
 # ♣♦♠♥
@@ -15,27 +17,29 @@ class Suit(enum.IntEnum):
 
 
 # Abstract class
-class PlayingCards:
+class PlayingCards(metaclass=abc.ABCMeta):
     """ This is an abstract class for playing cards """
-
-    def __init__(self, value: int, suit: Suit):
-        self.value = value
-        self.suit = suit
 
     def __str__(self):
         """ Returns a readable format of value and suit  """
         return "{}, {}".format(self.value, self.suit)
 
+    @abc.abstractmethod
     def get_value(self):
         """ Returns the value of the card """
         raise NotImplementedError("Derived class did not override this method")
 
+    @abc.abstractmethod
     def get_suit(self):
         """ Returns the suit of the card """
         raise NotImplementedError("Derived class did not override this method")
 
 
 class NumberedCard(PlayingCards):
+    def __init__(self, value: int, suit: Suit):
+        self.value = value
+        self.suit = suit
+
     def get_value(self):
         return self.value
 
@@ -103,16 +107,82 @@ class AceCard(PlayingCards):
         return self.suit
 
 
-
 class Hand:
     def __init__(self):
         self.cards = []
 
-    def add_card(self, card):
-        self.cards.append(card)
+    def __str__(self):
+        """ Returns a readable format of value and suit  """
+        text = ""
+        for card in self.cards:
+            text += str(card) + "\n"
+        return text
 
-    def remove_card(self):
+    def add_card(self, card):
+        return self.cards.append(card)
+
+    def remove_card(self, index):
         pass
 
     def sort_cards(self):
         pass
+
+
+class StandardDeck:
+    """ Text"""
+
+    def __init__(self):
+        self.card = None
+        self.deck = []
+
+    def __str__(self):
+        """ Returns a readable format of value and suit  """
+        text = ""
+        for card in self.deck:
+            text += str(card) + "\n"
+        return text
+
+    def create_deck(self):
+        # Create the numbered cards
+        for i in range(2, 11):
+            card = NumberedCard(i, Suit.clubs)
+            deck.append((card.value, card.suit))
+        for i in range(2, 11):
+            card = NumberedCard(i, Suit.diamonds)
+            deck.append((card.value, card.suit))
+        for i in range(2, 11):
+            card = NumberedCard(i, Suit.spades)
+            deck.append((card.value, card.suit))
+        for i in range(2, 11):
+            card = NumberedCard(i, Suit.hearts)
+            deck.append((card.value, card.suit))
+
+        return deck
+
+    def shuffle(self):
+        return random.shuffle(self.deck)
+
+
+# full_deck = create_deck()
+deck = []
+
+card_1 = NumberedCard(2, Suit.spades)
+card_2 = NumberedCard(2, Suit.hearts)
+
+jack_1 = JackCard(Suit.spades)
+
+deck.append((card_1.get_value(), (card_1.get_suit())))
+deck.append((card_2.get_value(), (card_2.get_suit())))
+
+print("card 1:", card_1)
+print("card 2:", card_2)
+# print("Deck:", deck)
+# print(deck[0] < deck[1])
+hand = Hand()
+hand.add_card(card_1)
+hand.add_card(card_2)
+print(hand)
+
+full_deck = StandardDeck()
+print(full_deck.create_deck(), '\n')
+print(random.shuffle(full_deck.create_deck()))
