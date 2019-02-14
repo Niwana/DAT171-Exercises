98527bce7dcbfc79970067e3f36f163e44c594ec
Niwana <martin.vanky@gmail.com>
2019-02-14 17:45:53 +0100

    Add files via upload

--- Exercise 2/cardlib.py
+++ Exercise 2/cardlib.py
@@ -20,6 +20,10 @@ class Suit(enum.IntEnum):
 class PlayingCards(metaclass=abc.ABCMeta):
     """ This is an abstract class for playing cards """
 
+    def __init__(self):
+        self.value = None
+        self.suit = None
+
     def __str__(self):
         """ Returns a readable format of value and suit  """
         return "{}, {}".format(self.value, self.suit)
@@ -36,6 +40,7 @@ class PlayingCards(metaclass=abc.ABCMeta):
 
 
 class NumberedCard(PlayingCards):
+    """ Description """
     def __init__(self, value: int, suit: Suit):
         self.value = value
         self.suit = suit
@@ -127,6 +132,25 @@ class Hand:
     def sort_cards(self):
         pass
 
+    def best_poker_hand(self):  # best_poker_hand(self, cards=[]):
+        if PokerHand.two_pair(self) == True:
+            print("Two pairs:", PokerHand.cards)
+        elif PokerHand.one_pair(self) == True:
+            print("One pair:", PokerHand.cards)
+        elif PokerHand.high_card(self) == True:
+            print("High cards:", PokerHand.cards)
+
+
+'''
+def best_hand(onHand):
+    if PokerHand.two_pair(onHand) == True:
+        print("Two pairs:", PokerHand.cards)
+    elif PokerHand.one_pair(onHand) == True:
+        print("One pair:", PokerHand.cards)
+    elif PokerHand.high_card(onHand) == True:
+        print("High cards:", PokerHand.cards)
+'''
+
 
 class StandardDeck:
     """ Text"""
@@ -163,6 +187,85 @@ class StandardDeck:
         return random.shuffle(self.deck)
 
 
+class PokerHand:
+    def __init__(self, cards):
+        self.cards = cards
+
+    def high_card(self):
+        """ Takes a list of tuples and return the biggest tuple. Starts with the first element, than the second """
+        highest_card = ()
+        for i in range(len(self)):
+            if self[i] > highest_card:
+                highest_card = self[i]
+            else:
+                pass
+        PokerHand.cards = highest_card  # Makes it possible to access the cards somewhere else
+        return True
+
+    def one_pair(self):
+        """ Return true if cards_to_evaluate has a pair in it """
+        pairs = []
+        for i in range(len(self)):
+            for j in range(len(self)):
+                # Check if the value of card i and card j is the same and whether or not they are not already in
+                # the list of pairs
+                if self[i][0] == self[j][0] and self[i][1] != self[j][1] and (
+                        self[i] not in pairs or self[j] not in pairs):
+                    pairs += self[i], self[j]
+        # Check if the hand only has one pair or multiple
+        if len(pairs) == 2:
+            PokerHand.cards = pairs
+            return True
+        else:
+            return False
+
+    def two_pair(self):
+        pairs = []
+        for i in range(len(self)):
+            for j in range(len(self)):
+                # Check if the value of card i and card j is the same and whether or not they are not already in
+                # the list of pairs
+                if self[i][0] == self[j][0] and self[i][1] != self[j][1] and (
+                        self[i] not in pairs or self[j] not in pairs):
+                    pairs += self[i], self[j]
+        # return pairs
+        if len(pairs) == 4:
+            PokerHand.cards = pairs
+            return True
+        else:
+            return False
+
+    def three_of_a_kind(self):
+        pass
+
+    def straight(self):
+        pass
+
+    def flush(self):
+        pass
+
+    def full_house(self):
+        pass
+
+    def four_of_a_kind(self):
+        pass
+
+    def straight_flush(self):
+        pass
+
+
+community_cards = [(4, Suit.spades), (3, Suit.hearts), (5, Suit.hearts), (6, Suit.spades), (10, Suit.spades)]
+player_1_cards = [(2, Suit.spades), (7, Suit.hearts)]
+cards_to_evaluate = community_cards + player_1_cards
+
+# print("Highest card:", PokerHand.high_card(onHand))
+# print("Pair:", PokerHand.one_pair(onHand))
+# print("Two pairs:", PokerHand.two_pair(onHand))
+
+
+Hand.best_poker_hand(cards_to_evaluate)
+
+'''
 # full_deck = create_deck()
 deck = []
 
@@ -186,3 +289,4 @@ print(hand)
 full_deck = StandardDeck()
 print(full_deck.create_deck(), '\n')
 print(random.shuffle(full_deck.create_deck()))
+'''
