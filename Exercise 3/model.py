0300c05d1e17a421e7389b1c232de42f153ea701
Niwana <martin.vanky@gmail.com>
2019-02-28 20:23:19 +0100

    Add files via upload

--- Exercise 3/model.py
+++ Exercise 3/model.py
@@ -1,5 +1,31 @@
 import cardlib
 
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import sys
+
+
+class Player:
+    def __init__(self):
+        self.cards = ['QS', '7C']
+        self.marked_cards = [False] * len(self.cards)
+        # self.credits = 100
+        # self.folded = False
+        self.cb = None
+
+
+class CommunityCards(QObject):
+    new_card = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.cards = ['2S', '3S', '4S']
+
+    def add_card(self):
+        self.cards.append('2H')
+        self.new_card.emit()
+
 
 class buttons():
     def __init__(self):
@@ -7,4 +33,3 @@ class buttons():
 
     def print_click(self):
         print('klick')
-
