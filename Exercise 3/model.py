99a3cec161cecd519239eadc258db6bc9e4d3512
Niwana <martin.vanky@gmail.com>
2019-03-02 13:40:09 +0100

    Add files via upload

--- Exercise 3/model.py
+++ Exercise 3/model.py
@@ -6,11 +6,37 @@ from PyQt5.QtWidgets import *
 import sys
 
 
+
+class TexasHoldEm(QObject):
+    new_credits = pyqtSignal()
+    new_pot = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.players = ['Name 1', 'Name 2']
+        self.credits = [1000, 1000]
+        self.pot = 0
+
+    def call(self):
+        pass
+
+    def bet(self, amount):
+        self.pot += amount
+        self.credits[0] -= amount
+        #self.new_pot.emit()
+        self.new_credits.emit()
+        print("Credits:", self.credits)
+        print("Pot", self.pot)
+
+    def fold(self):
+        pass
+
+
 class Player:
     def __init__(self):
         self.cards = ['QS', '7C']
         self.marked_cards = [False] * len(self.cards)
-        # self.credits = 100
+        # self.credits = 1000
         # self.folded = False
         self.cb = None
 
